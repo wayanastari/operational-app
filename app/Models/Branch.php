c1e368454ba533863b487ef74e67becc1fedba32
Ni Wayan Astari <wayanastari509@gmail.com>
2025-09-26 11:10:09 +0800

    first commit

---
+++ app/Models/Branch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Branch extends Model
+{
+    protected $fillable = [
+        'branch_name',
+        'branch_address',
+    ];
+}
---
+++ database/migrations/2025_09_26_023850_create_vehicle__variants_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicle__variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_vehicle_type')->constrained(
+                table: 'vehicle_types', indexName: 'id'
+            )->onUpdate('cascade')->onDelete('cascade');
+            $table->string('vehicle_variant');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicle__variants');
+    }
+};
